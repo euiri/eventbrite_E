e0d77e1115398d3eb1dac6a2a336134208eea48d
euiri <57123626+euiri@users.noreply.github.com>
2024-01-18 21:32:28 -0800

    routes, errorMiddleware in backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import eventRoutes from './routes/eventsRoutes.js' 
+import eventRoutes from './routes/eventRoutes.js' 
+import errorHandler from './middleware/errorMiddleware.js'
 
+const app = express()
 dotenv.config()
 connectDB()
 
-const app = express()
 app.use('/api/events',eventRoutes)
+
+app.use(errorHandler)
 
 
 app.listen(5000, console.log ("5000 is well listening."))
